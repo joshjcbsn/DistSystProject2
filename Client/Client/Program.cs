d4e7f2f2c9edbe0d2d4921fedcae4c063be47681
joshjcbsn <joshjcbsn@gmail.com>
2016-11-11 18:28:00 -0500

    client

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace Client
 {
+    public struct TCPConfig
+    {
+        public string dns;
+        public string ip;
+        public int port;
+        public TCPConfig(string DNS, string IP, int P)
+        {
+            dns = DNS;
+            ip = IP;
+            port = P;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //create connection to server
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                //create new thread that sends message and waits for response
+                    //create listener
+                    //send tcp message
+                    //start listener 
+                //loop continues to listen for UI
+            }
         }
     }
 }
---
+++ Client/Client/TCP.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class TCP
+    {
+        public TCP()
+        {
+        }
+
+        /// <summary>
+        /// Reads message. Blocks calling thread until the message has been read
+        /// </summary>
+        /// <param name="client">TCP client recieving message</param>
+        /// <returns>message</returns>
+        public string getMessage(TcpClient client)
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+                string data = null;
+                Console.WriteLine("Connected");
+                NetworkStream stream = client.GetStream();
+                int i;
+                // Loop to receive all the data sent by the client.
+                i = stream.Read(bytes, 0, bytes.Length);
+                while (i != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine(String.Format("Received: {0}", data));
+                    // Process the data sent by the client.
+                    i = stream.Read(bytes, 0, bytes.Length);
+                }
+                return data;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Sends TCP message to address specified in config 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="config"></param>
+        public void sendMessage(string msg, TCPConfig config)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient(config.dns, config.port))
+                {
+                    try
+                    {
+                        using (NetworkStream stream = client.GetStream())
+                        {
+                            byte[] msgBytes = Encoding.ASCII.GetBytes(msg);
+                            stream.Write(msgBytes, 0, msgBytes.Length);
+                        }
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
