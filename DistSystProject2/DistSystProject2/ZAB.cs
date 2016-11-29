b337bb09941cac5401f3020879a141b73153dba1
joshjcbsn <joshjcbsn@gmail.com>
2016-11-29 18:26:07 -0500

    public

--- DistSystProject2/DistSystProject2/ZAB.cs
+++ DistSystProject2/DistSystProject2/ZAB.cs
@@ -275,7 +275,7 @@ namespace Server
             catch (Exception ex)
             {
 
-                Console.WriteLine("sendProposal {0} {1}",ex.Message, ex.Source);
+                Console.WriteLine("sendProposal {0} {1}",ex.Message, ex.StackTrace);
                 if (tcp == servers[leader])
                 {
                     holdElection();
